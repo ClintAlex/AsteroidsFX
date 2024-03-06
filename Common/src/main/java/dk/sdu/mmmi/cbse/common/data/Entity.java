4984cd2fea709a316480d37db1410f3209192263
ClintAlex <clintbott@hotmail.com>
2024-03-06 16:29:35 +0100

    Bullet working from his example

--- Common/src/main/java/dk/sdu/mmmi/cbse/common/data/Entity.java
+++ Common/src/main/java/dk/sdu/mmmi/cbse/common/data/Entity.java
@@ -6,12 +6,13 @@ import java.util.UUID;
 public class Entity implements Serializable {
 
     private final UUID ID = UUID.randomUUID();
-    
+
     private double[] polygonCoordinates;
     private double x;
     private double y;
     private double rotation;
-            
+    private float radius;
+
 
     public String getID() {
         return ID.toString();
@@ -25,7 +26,7 @@ public class Entity implements Serializable {
     public double[] getPolygonCoordinates() {
         return polygonCoordinates;
     }
-       
+
 
     public void setX(double x) {
         this.x =x;
@@ -35,7 +36,7 @@ public class Entity implements Serializable {
         return x;
     }
 
-    
+
     public void setY(double y) {
         this.y = y;
     }
@@ -51,6 +52,12 @@ public class Entity implements Serializable {
     public double getRotation() {
         return rotation;
     }
-        
 
+    public void setRadius(float radius) {
+        this.radius = radius;
+    }
+
+    public float getRadius() {
+        return this.radius;
+    }
 }
