3523041aa376db141e300f367651cbfd2c78fef5
Clint <clintbott@hotmail.com>
2024-04-02 14:05:28 +0200

    Start of asteroids

---
+++ Asteroid/src/main/java/dk/sdu/mmmi/cbse/Asteroid/App.java
@@ -0,0 +1,9 @@
+package dk.sdu.mmmi.cbse.Asteroid;
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ Asteroid/src/main/java/module-info.java
@@ -0,0 +1,9 @@
+//import dk.sdu.mmmi.cbse.common.services.IEntityProcessingService;
+//import dk.sdu.mmmi.cbse.common.services.IGamePluginService;
+
+module Asteroid {
+    requires Common;
+    //provides IGamePluginService with dk.sdu.mmmi.cbse.bulletsystem.;
+    //provides IEntityProcessingService with dk.sdu.mmmi.cbse.bulletsystem.;
+    //provides Asteroids with dk.sdu.mmmi.cbse.bulletsystem.;
+}
